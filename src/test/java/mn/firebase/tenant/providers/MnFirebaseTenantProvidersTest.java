1c48d970765cc97cadad5d26f73678007a8e68a9
Jason Schindler <schindlerj@objectcomputing.com>
2021-06-14 11:23:43 -0500

    New project

---
+++ src/test/java/mn/firebase/tenant/providers/MnFirebaseTenantProvidersTest.java
@@ -0,0 +1,21 @@
+package mn.firebase.tenant.providers;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class MnFirebaseTenantProvidersTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
